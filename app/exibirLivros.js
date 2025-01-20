64034d59b0b640e7446ae8969d6533b6250cabf2
DWinst0n <daviwinstonsm@gmail.com>
2025-01-20 14:11:35 -0300

    adição de verificação de disponibilidade

--- app/exibirLivros.js
+++ app/exibirLivros.js
@@ -1,16 +1,20 @@
-export const livrosContainer = document.getElementById("livros");
+const livrosContainer = document.getElementById("livros");
 export function exibirLivros(listaLivros) {
-    listaLivros.forEach(livro => {
-        livrosContainer.innerHTML += `<div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-        <h2 class="livro__titulo">${livro.titulo}</h2>
-        <p class="livro__autor">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
-      </div>`
-    });
+  
+  livrosContainer.innerHTML = "";
+  listaLivros.forEach(livro => {
+    const estaDisponivel = livro.quantidade > 0;
+    let livroHTML = `<div class="livro">
+    <img class="livro__imagens ${estaDisponivel? "" : "indisponivel"}" src="${livro.imagem}" alt="${livro.alt}" />
+    <h2 class="livro__titulo">${livro.titulo}</h2>
+    <p class="livro__autor">${livro.autor}</p>
+    <p class="livro__preco" id="preco">${estaDisponivel? "R$" + livro.preco.toFixed(2) : "Indisponível"}</p>
+    <div class="tags">
+      <span class="tag">${livro.categoria}</span>
+    </div>
+  </div>`
+    livrosContainer.innerHTML += livroHTML;
+});
 }
 
 export function botaoRedefinirLivros (listaOriginal) {
@@ -25,9 +29,9 @@ export function botaoRedefinirLivros (listaOriginal) {
     listaBotoes.appendChild(itemBotao);
 
     botaoRedefinir.addEventListener("click", () => {
-        livrosContainer.innerHTML = "";
-        exibirLivros(listaOriginal);
-        botaoRedefinir.remove();
+      livrosContainer.innerHTML = ""
+      exibirLivros(listaOriginal);
+      botaoRedefinir.remove();
     });
 }
 }
